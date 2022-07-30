efb3dbf2d0f65c3aaf4a4cfd934cb02a96d49241
Fillipe143 <fillipedev.contato@gmail.com>
2022-07-30 11:20:00 -0300

    game updated

--- tests/scripts/objects/Player.js
+++ tests/scripts/objects/Player.js
@@ -1,5 +1,7 @@
 import BaseObject from '../../../src/game-objects/BaseObject.js'
 import Sprite from '../../../src/game-objects/Sprite.js'
+import { keyboard_check } from '../../../src/scripts/keyboard.js'
+
 
 export default class Player extends BaseObject {
     onCreate() {
@@ -7,5 +9,15 @@ export default class Player extends BaseObject {
             ['/tests/assets/sprites/tank-0.png'],
             { width: 66, height: 127 }
         )
+
+        this.speed = 3
+    }
+
+    onUpdate() {
+        const hdir = keyboard_check('d') - keyboard_check('a')
+        const vdir = keyboard_check('s') - keyboard_check('w')
+
+        this.coord.x += hdir * this.speed
+        this.coord.y += vdir * this.speed
     }
 }
