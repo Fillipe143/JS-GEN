61894b551ca6ae06241ab3275c2b87b0f545760e
Fillipe143 <fillipedev.contato@gmail.com>
2022-07-30 11:53:53 -0300

    updated sprite render

--- src/utils/SpriteRender.js
+++ src/utils/SpriteRender.js
@@ -6,12 +6,36 @@ import Sprite from '../game-objects/Sprite.js'
  * @param {Sprite} sprite 
  */
 export default function renderSprite(ctx, coord, sprite) {
-    const dimension = sprite.dimension
+    const dimension = {
+        width: sprite.dimension.width * sprite.scale.x,
+        height: sprite.dimension.height * sprite.scale.y
+    }
 
     const size = Math.sqrt(
-        Math.pow(dimension.width * sprite.scale.x, 2) +
-        Math.pow(dimension.height * sprite.scale.y, 2)
+        Math.pow(dimension.width, 2) +
+        Math.pow(dimension.height, 2)
     )
 
-    ctx.drawImage(sprite.getFrame(), coord.x, coord.y, dimension.width, dimension.height)
+    const canvas = document.createElement('canvas')
+    const nCtx = canvas.getContext('2d')
+    canvas.width = canvas.height = size
+
+
+    nCtx.fillRect(0, 0, size, size)
+
+    nCtx.translate(size / 2, size / 2)
+    nCtx.rotate(sprite.angle * Math.PI / 180)
+
+    nCtx.drawImage(
+        sprite.getFrame(),
+        -dimension.width / 2, -dimension.height / 2,
+        dimension.width, dimension.height
+    )
+
+
+    ctx.drawImage(
+        canvas,
+        coord.x - size / 2 + dimension.width / 2,
+        coord.y - size / 2 + dimension.height / 2
+    )
 }
